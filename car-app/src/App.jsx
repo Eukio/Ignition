579ab6006d2c750791c089d93e504f13594efa16
Eucharist Tan <Euki16321@gmail.com>
2025-11-08 16:31:37 -0600

    carousel component

--- car-app/src/App.jsx
+++ car-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Carousel from './components/Carousel.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Say Yar to the Car</h1>
+    <h1>Ignition</h1>
+    <Carousel />
+
     </>
   )
 }
